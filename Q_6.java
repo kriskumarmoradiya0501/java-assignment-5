debb24ac71445c218e068d38cd1018f4e26db64a
krish moradiya <140172827+kriskumarmoradiya0501@users.noreply.github.com>
2024-08-08 07:40:40 +0530

    Add files via upload

--- Q_6.java
+++ Q_6.java
@@ -1,59 +1,63 @@
-abstract class Person1 {
+import java.util.Scanner;
+
+abstract class Person2 {
     abstract void accept();
     abstract void display();
 }
 
-class Employee1 extends Person1 {
+class Employee2 extends Person2 {
     String emp_no;
     String emp_name;
-    String emp_add;
+    String address;
 
     void accept() {
-    }
-
-    void accept(String emp_no, String emp_name, String emp_add) {
-        this.emp_no = emp_no;
-        this.emp_name = emp_name;
-        this.emp_add = emp_add;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Employee Number: ");
+        emp_no = scanner.nextLine();
+        System.out.print("Enter Employee Name: ");
+        emp_name = scanner.nextLine();
+        System.out.print("Enter Employee Address: ");
+        address = scanner.nextLine();
     }
 
     void display() {
         System.out.println("Employee Number: " + emp_no);
         System.out.println("Employee Name: " + emp_name);
-        System.out.println("Employee Address: " + emp_add);
+        System.out.println("Employee Address: " + address);
     }
 }
 
-class Worker1 extends Person1 {
-    String work_no;
-    String work_name;
-    String work_add;
+class Worker2 extends Person2 {
+    String name;
+    int working_hour;
 
     void accept() {
-    }
-
-    void accept(String work_no, String work_name, String work_add) {
-        this.work_no = work_no;
-        this.work_name = work_name;
-        this.work_add = work_add;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Worker Name: ");
+        name = scanner.nextLine();
+        System.out.print("Enter Working Hours: ");
+        working_hour = scanner.nextInt();
     }
 
     void display() {
-        System.out.println("Worker Number: " + work_no);
-        System.out.println("Worker Name: " + work_name);
-        System.out.println("Worker Address: " + work_add);
+        System.out.println("Worker Name: " + name);
+        System.out.println("Working Hours: " + working_hour);
     }
 }
 
 public class Q_6 {
     public static void main(String[] args) {
-        Employee1 e = new Employee1();
-        Worker1 w = new Worker1();
+        Person2 employee = new Employee2();
+        Person2 worker = new Worker2();
 
-        e.accept("22BCA090", "Harmin", "Ashirwad Hostel");
-        w.accept("22BCA128", "Sahil", "Gurukul");
+        System.out.println("Enter Employee Details:");
+        employee.accept();
+        System.out.println("\nEmployee Details:");
+        employee.display();
 
-        e.display();
-        w.display();
+        System.out.println("\nEnter Worker Details:");
+        worker.accept();
+        System.out.println("\nWorker Details:");
+        worker.display();
     }
 }

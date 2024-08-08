29323abe644b6c12827dd848451844b2798fdca3
krish moradiya <140172827+kriskumarmoradiya0501@users.noreply.github.com>
2024-08-08 07:58:00 +0530

    Add files via upload

--- Q_9.java
+++ Q_9.java
@@ -1,30 +1,25 @@
-// Interface Shape
 interface Shape1 {
     void drawshape();
 }
 
-// Class Rectangle implements Shape
 class Rectangle1 implements Shape1 {
     public void drawshape() {
         System.out.println("Rectangle");
     }
 }
 
-// Class Triangle implements Shape
 class Triangle1 implements Shape1 {
     public void drawshape() {
         System.out.println("Triangle");
     }
 }
 
-// Class Hexagon implements Shape
 class Hexagon1 implements Shape1 {
     public void drawshape() {
         System.out.println("Hexagon");
     }
 }
 
-// Main class
 public class Q_9 {
     public static void main(String[] args) {
         Shape1 r = new Rectangle1();
